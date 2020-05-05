9c67fd4625331ab5cb6d41fa73cacbb6dcc332d3
Hathra <marianb95@yahoo.com>
2020-05-05 20:36:30 +0300

    Added walls, ball, goal objs + ball movement.

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
-const { Engine, Render, Runner, World, Bodies } = Matter;
+const { Engine, Render, Runner, World, Bodies, Body, Events } = Matter;
 
-const cells = 3;
-const width = 400;
-
-const height = 400;
+const cells = 5;
+const width = 600;
+const height = 600;
+const unitLength = width / cells;
 
 const engine = Engine.create();
+engine.world.gravity.y = 0;
 const { world } = engine;
 const render = Render.create({
 	element: document.body,
 	engine: engine,
 	options: {
-		wireframes: false,
+		wireframes: true,
 		width,
 		height
 	}
@@ -23,10 +24,10 @@ Runner.run(Runner.create(), engine);
 //Walls
 
 const walls = [
-	Bodies.rectangle(width / 2, 0, width, 40, { isStatic: true }),
-	Bodies.rectangle(width / 2, height, width, 40, { isStatic: true }),
-	Bodies.rectangle(0, height / 2, 40, height, { isStatic: true }),
-	Bodies.rectangle(width, height / 2, 40, height, { isStatic: true })
+	Bodies.rectangle(width / 2, 0, width, 5, { isStatic: true }),
+	Bodies.rectangle(width / 2, height, width, 5, { isStatic: true }),
+	Bodies.rectangle(0, height / 2, 5, height, { isStatic: true }),
+	Bodies.rectangle(width, height / 2, 5, height, { isStatic: true })
 ];
 
 World.add(world, walls);
@@ -56,6 +57,7 @@ const horizontals = Array(cells - 1).fill(null).map(() => Array(cells).fill(fals
 const startRow = Math.floor(Math.random() * cells);
 const startCol = Math.floor(Math.random() * cells);
 
+//Function that goes through every cell
 const iterateThroughCell = (row, col) => {
 	//Return if the cell is visited
 	if (grid[row][col]) {
@@ -97,3 +99,92 @@ const iterateThroughCell = (row, col) => {
 };
 
 iterateThroughCell(startRow, startCol);
+
+//Making horizontal walls
+horizontals.forEach((row, rowIndex) => {
+	row.forEach((open, columnIndex) => {
+		if (open) {
+			return;
+		}
+
+		const wall = Bodies.rectangle(
+			columnIndex * unitLength + unitLength / 2,
+			rowIndex * unitLength + unitLength,
+			unitLength,
+			5,
+			{
+				label: 'wall',
+				isStatic: true
+			}
+		);
+
+		World.add(world, wall);
+	});
+});
+
+//Making vertical walls
+verticals.forEach((row, rowIndex) => {
+	row.forEach((open, columnIndex) => {
+		if (open) {
+			return;
+		}
+
+		const wall = Bodies.rectangle(
+			columnIndex * unitLength + unitLength,
+			rowIndex * unitLength + unitLength / 2,
+			5,
+			unitLength,
+			{
+				label: 'wall',
+				isStatic: true
+			}
+		);
+
+		World.add(world, wall);
+	});
+});
+
+//Goal creation
+const goal = Bodies.rectangle(width - unitLength / 2, height - unitLength / 2, unitLength * 0.7, unitLength * 0.7, {
+	isStatic: true,
+	label: 'goal'
+});
+
+World.add(world, goal);
+
+//Ball creation
+const ball = Bodies.circle(unitLength / 2, unitLength / 2, unitLength * 0.3, { label: 'ball' });
+World.add(world, ball);
+
+//Handling keypress
+const { x, y } = ball.velocity;
+document.addEventListener('keydown', (event) => {
+	if (event.keyCode === 87) {
+		Body.setVelocity(ball, { x, y: y - 5 });
+	}
+	if (event.keyCode === 68) {
+		Body.setVelocity(ball, { x: x + 5, y });
+	}
+	if (event.keyCode === 83) {
+		Body.setVelocity(ball, { x, y: y + 5 });
+	}
+	if (event.keyCode === 65) {
+		Body.setVelocity(ball, { x: x - 5, y });
+	}
+});
+
+//Win condition
+Events.on(engine, 'collisionStart', (event) => {
+	event.pairs.forEach((collision) => {
+		const labels = [ 'ball', 'goal' ];
+
+		if (labels.includes(collision.bodyA.label) && labels.includes(collision.bodyB.label)) {
+			world.gravity.y = 1;
+			world.bodies.forEach((body) => {
+				if (body.label === 'wall') {
+					Body.setStatic(body, false);
+				}
+			});
+		}
+	});
+});
